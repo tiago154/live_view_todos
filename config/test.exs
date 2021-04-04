5012bc8fad310541ee3d6c862367e6cd4b5cc5d6
Tiago Silva <tiago.toya@hotmail.com>
2021-04-04 12:12:12 -0300

    ci: adicionando variavel de ambiente para o teste

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :live_view_todos, LiveViewTodos.Repo,
   username: "postgres",
   password: "postgres",
   database: "live_view_todos_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: System.get_env("DB_HOST", "localhost"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
