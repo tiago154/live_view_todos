96fc048c4c0f40cf17a45ca9e65061ca7e251728
Tiago Silva <tiago.toya@hotmail.com>
2021-04-04 00:09:41 -0300

    feat: commit inicial

---
+++ lib/live_view_todos/todos.ex
@@ -0,0 +1,118 @@
+defmodule LiveViewTodos.Todos do
+  @moduledoc """
+  The Todos context.
+  """
+
+  import Ecto.Query, warn: false
+  alias LiveViewTodos.Repo
+
+  alias LiveViewTodos.Todos.Todo
+
+  @topic inspect(__MODULE__)
+
+  def subscribe do
+    Phoenix.PubSub.subscribe(LiveViewTodos.PubSub, @topic)
+  end
+
+  defp broadcast_change({:ok, result}, event) do
+    Phoenix.PubSub.broadcast(LiveViewTodos.PubSub, @topic, {__MODULE__, event, result})
+  end
+
+  @doc """
+  Returns the list of todos.
+
+  ## Examples
+
+      iex> list_todos()
+      [%Todo{}, ...]
+
+  """
+  def list_todos do
+    Repo.all(Todo)
+  end
+
+  @doc """
+  Gets a single todo.
+
+  Raises `Ecto.NoResultsError` if the Todo does not exist.
+
+  ## Examples
+
+      iex> get_todo!(123)
+      %Todo{}
+
+      iex> get_todo!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_todo!(id), do: Repo.get!(Todo, id)
+
+  @doc """
+  Creates a todo.
+
+  ## Examples
+
+      iex> create_todo(%{field: value})
+      {:ok, %Todo{}}
+
+      iex> create_todo(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_todo(attrs \\ %{}) do
+    %Todo{}
+    |> Todo.changeset(attrs)
+    |> Repo.insert()
+    |> broadcast_change([:todo, :created])
+  end
+
+  @doc """
+  Updates a todo.
+
+  ## Examples
+
+      iex> update_todo(todo, %{field: new_value})
+      {:ok, %Todo{}}
+
+      iex> update_todo(todo, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_todo(%Todo{} = todo, attrs) do
+    todo
+    |> Todo.changeset(attrs)
+    |> Repo.update()
+    |> broadcast_change([:todo, :updated])
+  end
+
+  @doc """
+  Deletes a todo.
+
+  ## Examples
+
+      iex> delete_todo(todo)
+      {:ok, %Todo{}}
+
+      iex> delete_todo(todo)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_todo(%Todo{} = todo) do
+    todo
+    |> Repo.delete()
+    |> broadcast_change([:todo, :deleted])
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking todo changes.
+
+  ## Examples
+
+      iex> change_todo(todo)
+      %Ecto.Changeset{data: %Todo{}}
+
+  """
+  def change_todo(%Todo{} = todo, attrs \\ %{}) do
+    Todo.changeset(todo, attrs)
+  end
+end
---
+++ lib/live_view_todos_web/live/todo_live.ex
@@ -0,0 +1,36 @@
+defmodule LiveViewTodosWeb.TodoLive do
+  use LiveViewTodosWeb, :live_view
+  alias LiveViewTodos.Todos
+
+  def mount(_params, _session, socket) do
+    Todos.subscribe()
+
+    {:ok, fetch(socket)}
+  end
+
+  def handle_event("add", %{"todo" => todo}, socket) do
+    Todos.create_todo(todo)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("toggle_done", %{"id" => id}, socket) do
+    todo = Todos.get_todo!(id)
+    Todos.update_todo(todo, %{done: !todo.done})
+    {:noreply, socket}
+  end
+
+  def handle_event("delete", %{"id" => id}, socket) do
+    todo = Todos.get_todo!(id)
+    Todos.delete_todo(todo)
+    {:noreply, socket}
+  end
+
+  def handle_info({Todos, [:todo | _], _}, socket) do
+    {:noreply, fetch(socket)}
+  end
+
+  defp fetch(socket) do
+    assign(socket, todos: Todos.list_todos())
+  end
+end
